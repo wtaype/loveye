b4ded813c5a3d889a0b18f67a61b10f33d0b87ac
wtaype <wilder.taype16@gmail.com>
2026-01-28 18:30:26 -0500

    Actualizacion Principal v11.10.12

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig(({ mode }) => ({
-  base: '/loveye/',
+  base: mode === 'production' ? '/loveye/' : '/', // Firebase + GitHub Pages
   build: {
     outDir: 'dist',
     minify: 'esbuild',
