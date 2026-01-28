e0b2ec5790fbd1af4e17db8b100d5eceeeeafbb3
wtaype <wilder.taype16@gmail.com>
2026-01-28 18:28:15 -0500

    Actualizacion Principal v11.10.11

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig(({ mode }) => ({
-  base: '/', 
+  base: '/loveye/',
   build: {
     outDir: 'dist',
     minify: 'esbuild',
