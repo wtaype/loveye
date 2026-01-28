f764af55642db03a4cfa6e8fd6e7a0f87d3cc2b6
wtaype <wilder.taype16@gmail.com>
2026-01-28 15:32:37 -0500

    Actualizacion Principal v10.10.12

--- smiles/smile/firebase.js
+++ smiles/smile/firebase.js
@@ -1,4 +1,4 @@
-import { id } from './wii.js';
+import { id } from '../wii.js';
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
